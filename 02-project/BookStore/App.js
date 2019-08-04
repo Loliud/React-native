981e72c3e49cc398483696ab237f9f4faca9379e
Loliud <anhhaiquynhon123@gmail.com>
2019-08-04 16:08:06 +0700

    scoll view

--- 02-project/BookStore/App.js
+++ 02-project/BookStore/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import CategoryListItem from './components/CategoryListItem';
 import home from './assets/Home.png';
 import book from './assets/book.png';
 import cart from './assets/cart.png'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <CategoryListItem image={home} title="Home"/>
-      <CategoryListItem image={book} title="Products"/>
-      <CategoryListItem image={cart} title="Cart"/>
+    <View>
+      <ScrollView style={{paddingLeft: 16, paddingRight: 16}} contentContainerStyle={{marginTop: 200}}>
+        <CategoryListItem image={home} title="Home" />
+        <CategoryListItem image={book} title="Products" />
+        <CategoryListItem image={cart} title="Cart" />
+        <CategoryListItem image={home} title="Home" />
+        <CategoryListItem image={book} title="Products" />
+        <CategoryListItem image={cart} title="Cart" />
+        <CategoryListItem image={home} title="Home" />
+        <CategoryListItem image={book} title="Products" />
+        <CategoryListItem image={cart} title="Cart" />
+        <CategoryListItem image={home} title="Home" />
+        <CategoryListItem image={book} title="Products" />
+        <CategoryListItem image={cart} title="Cart" />
+      </ScrollView>
     </View>
   );
 }
